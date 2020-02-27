2377dcf644bd83bf81df0b59f58aa606b13a5258
marlieer <marlie_97@hotmail.ca>
2020-02-26 19:33:52 -0800

    adjusting gitignore and register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,6 +11,9 @@
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                            <p>
+                                Please complete small survey located <a href="https://ubc.ca1.qualtrics.com/jfe/form/SV_0HZ8YhHzCqwIdyl" target="_blank"><strong>Here</strong></a>
+                            </p>
                             <label for="name" class="col-md-4 control-label">Name</label>
 
                             <div class="col-md-6">
@@ -59,32 +62,6 @@
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                             </div>
                         </div>
-
-                        <div class="form-group" style="padding: 1em;">
-                            <p>
-                                I have read <a href="{{ asset('Consent-student-general.pdf') }}" target="popup" onclick="window.open('{{ asset('Consent-student-general.pdf') }}', 'popup','width=600'); return false;"><strong>this Consent Form</strong></a>. I have had the opportunity to ask, and I have received answers
-                                to, any questions I had regarding the study and the use and disclosure of information about me
-                                for the study. I agree to take part in this study as a research participant. By my signature I affirm
-                                that I have received a copy of this Consent form
-                            </p>
-                            <table>
-                                <tr>
-                                    <td><strong>I am willing to provide access to my submitted course work</strong></td>
-                                    <td><input type="radio" value="true" name="consent_course_work" required/><strong>Yes</strong></td>
-                                    <td><input type="radio" value="false" name="consent_course_work" required/><strong>No</strong></td>
-                                </tr>
-                                <tr>
-                                    <td><strong>I am willing to provide access to my course grades associated with the submitted work</strong></td>
-                                    <td><input type="radio" value="true"  name="consent_grades" required/><strong>Yes</strong></td>
-                                    <td><input type="radio" value="false" name="consent_grades" required/><strong>No</strong></td>
-                                </tr>
-                            </table>
-                            <br/>
-                            <p>Your decision to participate in this study will in no way impact your course grades, class
-                                standing, or your relationship with UBC.
-                            </p>
-                        </div>
-
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
