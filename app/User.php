eaacd96d5f47b901a36282b8de87da3e8c6146f4
marlieer <marlie_97@hotmail.ca>
2020-03-19 09:28:40 -0700

    adding tracking on last login

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'last_login'
     ];
 
     /**
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function updateLastLogin()
+    {
+        $this->last_login = new DateTime();
+    }
 }
