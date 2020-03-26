3a42bd92d445adaa0e2937eada31e2f1f8febf76
marlieer <marlie_97@hotmail.ca>
2020-03-26 09:14:29 -0700

    mail config update to gmail

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'marlieellen97@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'ubco videos'),
     ],
 
     /*
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME'),
+    'username' => env('MAIL_USERNAME', 'marlieellen97@gmail.com'),
 
-    'password' => env('MAIL_PASSWORD'),
+    'password' => env('MAIL_PASSWORD', '97.Humoreske'),
 
     /*
     |--------------------------------------------------------------------------
